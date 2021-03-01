1af592bf5a6f05491e5840c460631f96d5fb685d
anjali13s <79659570+anjali13s@users.noreply.github.com>
2021-03-01 17:27:38 +0300

    Update InitializeCentroid.py

--- InitializeCentroid.py
+++ InitializeCentroid.py
@@ -1,10 +1,7 @@
 def InitializeMeans(items, k, Min, Max): 
     for i in range k: 
   
-     centroid[i] = uniform(Min[i]+1, Max[i]-1); 
-     if(EuclideanDistance(centroid[i],centroid[i-1])<0.1):
-         centroid[i] = uniform(Min[i]+1, Max[i]-1); 
-      
+     centroid[i] = uniform(Min[i]+1, Max[i]-1);     
         
      
     return centroid[]; 
