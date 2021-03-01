01c04591978a7e98c1f4ab3f6aa3a0d15dfe5f7d
stuti371 <stuti.khandelwal01@gmail.com>
2021-03-01 20:29:02 +0530

    AssignMean

--- K-MeansClustering.py
+++ K-MeansClustering.py
@@ -1,14 +1,28 @@
 
-def DistanceBetweenPoints(x1, x2, y1, y2):
-    distance = ((x2 - x1)**2 + (y2- y1)**2)**(0.5)
+def DistanceBetweenPoints(a, b):
+    S = 0;
+    for i in range(len(x)):
+        S = S + (a[i] - b[i])**2  #calculates sum of squared differences
+    distance = S**(0.5);
     return distance
 
-def AttributeMinMax(column): #input rows[i]
-    minimum = column[0];
-    maximum = column[0];
-    for i in range(len(column)): #looping over all records 
-        if(rows[i]<minimum):
-            minimum = column[i]  #reassign the value if new value is less than the current minimum value
-        if(rows[i]>maximum):
-            maximum = column[i] #reassign the value if new value is more than the current maximum value
+def AttributeMinMax(rows, n): #input rows[i]
+    minimum = rows[0][n];
+    maximum = rows[0][n];
+    for i in range(len(rows)): #looping over all records 
+        if(rows[i][n]<minimum):
+            minimum = rows[i][n]  #reassign the value if new value is less than the current minimum value
+        if(rows[i][n]>maximum):
+            maximum = rows[i][n] #reassign the value if new value is more than the current maximum value
     return minimum, maximum 
+
+def Assign(means, item): #inputs array of means and selected item
+    minimum = 100000;  
+    index = -1;
+    for i in means[k]: 
+        distance = DistanceBetweenPoints(item, means[i])  #calculate Euclidian Distance
+        if(distance < minimum): 
+            minimum = distance;
+            index = i;     #finds the mean with the minimum distance from the item
+    return index
+
