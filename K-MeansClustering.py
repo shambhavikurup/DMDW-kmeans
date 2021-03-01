5f3c716a163992a71f528b798f19e37e30d86bc0
stuti371 <stuti.khandelwal01@gmail.com>
2021-03-01 21:32:14 +0530

    Updated CalculateMeans

--- K-MeansClustering.py
+++ K-MeansClustering.py
@@ -49,3 +49,32 @@ def UpdateCentroid(n, centroid, item): #n is the number of items in that cluster
         centroid[i] = round(m, 3); 
       
     return centroid;
+
+def CalculateMeans(k, items):
+  Attributes = [11, 13] #finding minimum and maximum value of the points in the dataset
+  for i in range(2):
+      Min[i], Max[i] = AttributeMinMax(rows, Atrributes[i]);
+      
+  means = InitializeMeans(k, Min, Max); #initiali
+  
+  clusterLength= [0 for i in range(len(means))]; #array to hold number of items in a cluster
+  belonging = [0 for i in range(len(items))]; #array to hold the cluster an item is in
+
+  for e in range(100000): 
+    noChange = True; 
+    for i in range(len(items)): 
+      item = items[i]; 
+      index = Assign(means,item); #assigning items into a cluster
+      clusterLength[index] = clusterLength[i] + 1; 
+      cSize = clusterLength[index]; 
+      means[index] = UpdateCentroid(cSize,means[index],item); 
+      
+      if(index != belongsTo[i]): 
+        noChange = False; 
+        belonging[i] = index; 
+        
+        
+    if (noChange): 
+            break; 
+  
+    return means;
