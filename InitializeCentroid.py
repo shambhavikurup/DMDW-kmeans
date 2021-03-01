501b4654d0d3d08999fa41697666d9e58a3a895e
anjali13s <79659570+anjali13s@users.noreply.github.com>
2021-03-01 17:49:04 +0300

    Update InitializeCentroid.py

--- InitializeCentroid.py
+++ InitializeCentroid.py
@@ -1,7 +1,5 @@
-def InitializeMeans(items, k, Min, Max): 
+def InitializeMeans( k, Min, Max): 
     
-    f = len(items[0]); 
-   
     for centroid in means[for j in range(k)]: 
         for i in range(len(centroid)): 
             centroid[i] = uniform(Min[i]+1, Max[i]-1); 
