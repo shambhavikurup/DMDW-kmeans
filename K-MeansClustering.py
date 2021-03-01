80c0861087cf5d2b073fb2656b0544661a1ee0c1
stuti371 <stuti.khandelwal01@gmail.com>
2021-03-01 21:01:37 +0530

    FindClusters added

--- K-MeansClustering.py
+++ K-MeansClustering.py
@@ -1,3 +1,4 @@
+from random import uniform
 
 def DistanceBetweenPoints(a, b):
     S = 0;
@@ -16,6 +17,14 @@ def AttributeMinMax(rows, n): #input rows[i]
             maximum = rows[i][n] #reassign the value if new value is more than the current maximum value
     return minimum, maximum 
 
+def InitializeMeans( k, Min, Max): 
+    
+    for centroid in means[k]: 
+        for i in range(len(centroid)): 
+            centroid[i] = uniform(Min[i]+1, Max[i]-1); 
+  
+    return means; 
+
 def Assign(means, item): #inputs array of means and selected item
     minimum = 100000;  
     index = -1;
@@ -26,3 +35,17 @@ def Assign(means, item): #inputs array of means and selected item
             index = i;     #finds the mean with the minimum distance from the item
     return index
 
+def FindClusters(means, items):
+    Clusters = [[] for i in range(k)];
+    for item in items:
+        index = Assign(means, item);  #finds index of centroid closest to item
+        Clusters[index].append(item);  #adds item to its cluster
+    return Clusters;
+
+def UpdateCentroid(n, centroid, item): #n is the number of items in that cluster, item is the datapoint in that cluster
+    for i in range(len(centroid)): 
+        m = centroid[i];  #taking in the value of the current centroid
+        m = (m*(n-1)+item[i])/float(n); #finding the average of the points and recalculating m
+        centroid[i] = round(m, 3); 
+      
+    return centroid;
